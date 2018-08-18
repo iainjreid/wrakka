0f9c3935da8c9d48247904f384f4f5a07ed9bcba
Iain Reid <iainreid382@gmail.com>
2018-08-18 20:37:08 +0100

    Remove unsed type

--- src/main/scala/com/candied/WrakkaRoute.scala
+++ src/main/scala/com/candied/WrakkaRoute.scala
@@ -3,7 +3,7 @@ package com.candied
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.{Directives, StandardRoute}
 
-trait WrakkaRoute[M[_]] extends WrakkaContext with Directives {
+trait WrakkaRoute extends WrakkaContext with Directives {
   def complete[T](result: WrakkaResult[T])(implicit toString: T => String): StandardRoute = {
     complete(result.value match {
       case Right(response) => HttpResponse(StatusCodes.OK, entity = HttpEntity(ContentTypes.`application/json`, toString(response)))
